dbb22ddb5674b6a5f2af2c4a18c589c29ca5e5f4
lpiguet <laurent.piguet@gmail.com>
2014-04-09 13:53:11 +0200

    - Updated with handlebar

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 var app = {
 
-    findByName: function() {
-        console.log('findByName');
-        this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
-        });
-    },
-
-
     showAlert: function (message, title) {
         if (navigator.notification) {
             navigator.notification.alert(message, null, title, 'OK');
@@ -22,14 +8,28 @@ var app = {
         }
     },
 
+    renderHomeView: function() {
+        $('body').html(this.homeTpl());
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
+    findByName: function() {
+        var self = this;
+        this.store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(self.employeeLiTpl(employees));
+        });
+    },
+
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function () { 
-                self.showAlert('Store Initialized', 'Info');
-            });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
 
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+
+        this.store = new MemoryStore(function () { 
+            self.renderHomeView();
+        });
+    }
 };
 
 app.initialize();
