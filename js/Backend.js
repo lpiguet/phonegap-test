be109345274ff9f4e9b1011738927a120703b693
lpiguet <laurent.piguet@gmail.com>
2014-04-28 11:07:47 +0200

    - Added missing file

---
+++ js/Backend.js
@@ -0,0 +1,22 @@
+function Backend (name, addr, endpoint, auth_endpoint) {
+
+    this.getAddr = function () {
+        return this.addr;
+    }
+
+    this.getTicket = function () {
+        return this.auth.getTicket();
+    }
+
+    if (window.location.hostname == 'local-'+addr) {
+        this.addr = 'http://local-'+addr+'/'+endpoint; // local version
+    } else if (window.location.hostname == 'local.'+addr) {
+        this.addr = 'http://local.'+addr+'/'+endpoint; // local version
+    } else {        this.addr = 'https://'+addr+'/'+endpoint; // deployed version
+    }
+
+    this.auth = new Auth (name, this.addr+auth_endpoint);
+
+//    console.log ('new '+ this.constructor.name + ': '+ JSON.stringify(this));
+
+}
